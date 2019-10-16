d06542423ae3ab8d5c3a9df3c2b12f3bfd156395
Kizito Njoku <nkizito57@gmail.com>
2019-10-16 17:05:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,35 +1,36 @@
 def new_hash
-  new_hash.new
+  hash = {}
 end
 
 def my_hash
-  my_hash = {:blue => "car", 
+  hash = {:blue => "car", 
     :red => "truck"
   }
 end
 
 def pioneer
- key = :name
-  pioneer[key]= "Grace Hopper"
+ 
+  hash = { :name => "Grace Hopper"}
 end
 
 def id_generator
-  :id.object_id
+  hash = {:id => 1}
+  
 end
 
 def my_hash_creator(key, value)
-  key = :name
-  value = "tree"
-  hash[key] =  value
+
+  p hash = { key => value }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] 
-    hash[key] += 1
+  if !hash[key]
+    hash = {key => 1}
   else 
-    hash[key] = 1
+    hash[key] += 1
+  end
 end
